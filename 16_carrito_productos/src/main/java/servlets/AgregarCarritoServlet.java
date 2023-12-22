daad0d3a27b07999e6d64fa83f590ffc4117c41b
Profesortarde <Profesortarde@A13E100>
2023-12-22 16:31:49 +0100

    ultimos cambios 16

--- 16_carrito_productos/src/main/java/servlets/AgregarCarritoServlet.java
+++ 16_carrito_productos/src/main/java/servlets/AgregarCarritoServlet.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,7 +13,7 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.Session;
 import model.Producto;
 
-
+@WebServlet("/AgregarCarritoServlet")
 public class AgregarCarritoServlet extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- 16_carrito_productos/src/main/java/servlets/EliminarCarritoServlet.java
+++ 16_carrito_productos/src/main/java/servlets/EliminarCarritoServlet.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.List;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Producto;
 
-
+@WebServlet("/EliminarCarritoServlet")
 public class EliminarCarritoServlet extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
